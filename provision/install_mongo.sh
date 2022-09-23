cbb25a57f9738e683f331f4ec6e7cb57b409cf7d
Franklin Ribeiro <franklin.ribe@gmail.com>
2022-09-23 20:18:08 -0300

    update mongodb version

--- provision/install_mongo.sh
+++ provision/install_mongo.sh
@@ -1,10 +1,10 @@
 echo "=================INSTALLING REPO KEY=================="
 
-sudo wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+sudo wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
 
 echo "=================INSTALLING MONGODB REPO=================="
 
-sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
 echo "=================UPDATE PACKAGES=================="
 
